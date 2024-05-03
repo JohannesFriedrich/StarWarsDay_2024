1cfbe44164a87619a4bcfb66d419e5a8cf51185f
JohannesFriedrich <johannes.friedrich@posteo.de>
2024-05-03 21:32:33 +0200

    gmx STUN

--- vader.py
+++ vader.py
@@ -113,7 +113,7 @@ webrtc_streamer(
         "audio": False
     },
     rtc_configuration={ 
-        "iceServers": [{"urls": ["stun:stun3.l.google.com:19302"]}]
+        "iceServers": [{"urls": ["stun:stun.gmx.net:3478"]}]
     },
     video_frame_callback=video_frame_callback
     )
