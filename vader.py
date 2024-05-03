8ff1b8c4ca38dc945b1185b6b64946970279455c
JohannesFriedrich <johannes.friedrich@posteo.de>
2024-05-03 19:46:33 +0200

    change stun server

--- vader.py
+++ vader.py
@@ -113,7 +113,7 @@ webrtc_streamer(
         "audio": False
     },
     rtc_configuration={ 
-        "iceServers": [{"urls": ["stun:stun1.l.google.com:19302"]}]
+        "iceServers": [{"urls": ["stun:stun.schlund.de"]}]
     },
     video_frame_callback=video_frame_callback
     )
